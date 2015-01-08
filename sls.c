ce4805f724b6413bc4b20089869e43992c399269
Iburinoc <iburinoc@gmail.com>
2015-01-08 13:36:15 -0500

    added exhaust

--- sls.c
+++ sls.c
@@ -15,16 +15,47 @@ const int fps = 100;
 char *rocket[];
 char *blank;
 
-void display(int pos, int offset, int h) {
+const int num_exhausts = 1000;
+const char *exhaust_chars = "*";
+const int num_exhaust_chars = 1;
+const int exhaust_speed = 4;
+
+struct exhaust {
+	int x, y, xv;
+	char c;
+};
+
+void display(int pos, int offset, int h, struct exhaust *exhausts) {
 	int i;
 	for(i = 0; i < h && i + pos < r_length; i++) {
-		mvprintw(i, offset - r_shake / 2, blank);
+		mvprintw(i, 0, blank);
 		mvprintw(i, offset, "%s", rocket[i + pos]);
 	}
 	for(; i < h; i++) {
-		mvprintw(i, offset, blank);
+		mvprintw(i, 0, blank);
+	}
+	if(pos + h > r_length) {
+		for(i = 0; i < num_exhausts; i++) {
+			mvprintw(r_length - pos + exhausts[i].y,
+				offset + exhausts[i].x + r_mid, "%c",
+				exhausts[i].c);
+		}
 	}
 	refresh();
+}
+
+void update_exhausts(struct exhaust *exhausts, int h, int v) {
+	int i;
+	for(i = 0; i < num_exhausts; i++) {
+		exhausts[i].y += exhaust_speed + v;
+		exhausts[i].x += exhausts[i].xv;
+		if(exhausts[i].y > h * 2 || exhausts[i].c == '\0') {
+			exhausts[i].y = rand() % 20;
+			exhausts[i].x = rand() % 26 - 13;
+			exhausts[i].xv = rand() % 3 - 1;
+			exhausts[i].c = exhaust_chars[rand() % num_exhaust_chars];
+		}
+	}
 }
 
 void rocket_launch(int w, int h) {
@@ -32,9 +63,12 @@ void rocket_launch(int w, int h) {
 	int t = 0;
 	const int sleep = 1000000 / fps;
 
+	struct exhaust exhausts[num_exhausts];
+	memset(exhausts, 0x00, sizeof(exhausts));
+
 	const int wait_time = fps;
 	while(t < wait_time) {
-		display(0, offset + rand() % r_shake - (r_shake/2), h);
+		display(0, offset + rand() % r_shake - (r_shake/2), h, exhausts);
 		t++;
 		usleep(sleep);
 	}
@@ -43,7 +77,8 @@ void rocket_launch(int w, int h) {
 	double v = 0;
 
 	while((int)pos < r_length + r_tail + h) {
-		display((int)pos, offset + rand() % r_shake - (r_shake/2), h);
+		update_exhausts(exhausts, h, (int) v);
+		display((int)pos, offset + rand() % r_shake - (r_shake/2), h, exhausts);
 		v += a;
 		pos += v;
 		usleep(sleep);
@@ -60,15 +95,15 @@ int main() {
 	/* init rand */
 	srand(time(NULL));
 
-	/* init blank line */
-	blank = malloc(50 + r_shake * 2 + 1);
-	memset(blank, ' ', 50 + r_shake * 2);
-
 	/* init ncurses */
 	int width, height;
 	initscr();
 	getmaxyx(stdscr, height, width);
 	curs_set(0);
+
+	/* init blank line */
+	blank = malloc(width + 1);
+	memset(blank, ' ', width);
 
 	/* run launch routine */
 	rocket_launch(width, height);
